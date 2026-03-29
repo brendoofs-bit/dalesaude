0305606d5b8f2b10823083a982b4ab0b99eb47a0
brendoofs-bit <brendoofs@gmail.com>
2026-03-29 20:51:40 -0300

    Update Home.tsx

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -1,39 +1,43 @@
 import React from 'react';
-// Importação das seções que compõem a Homepage
+// Importação de todas as seções (restaurando o ImageMarquee)
 import Hero from '@/components/Home/Hero';
+import ImageMarquee from '@/components/Home/ImageMarquee';
 import ServicesTabs from '@/components/Home/ServicesTabs';
 import ValuesMarquee from '@/components/Home/ValuesMarquee';
 import DalePlusTeaser from '@/components/Home/DalePlusTeaser';
 import AboutSection from '@/components/Home/AboutSection';
 import ReviewsSection from '@/components/Home/ReviewsSection';
 import LocationSection from '@/components/Home/LocationSection';
-import WhyChooseUs from '@/components/Shared/WhyChooseUs';
+import DaleIndicaSection from '@/components/Home/DaleIndicaSection';
 
 const Home: React.FC = () => {
   return (
     <main>
-      {/* 1. Seção de Impacto Inicial */}
+      {/* 1. Impacto Inicial (Banner principal) */}
       <Hero />
 
-      {/* 2. Prova Social e Diferenciais */}
-      <WhyChooseUs />
+      {/* 2. Faixa de Fotos da Clínica (O que você pensou em remover, mas agora voltou) */}
+      <ImageMarquee />
 
-      {/* 3. Abas de Serviços e Especialidades */}
+      {/* 3. Abas de Especialidades e Exames (O coração do site) */}
       <ServicesTabs />
 
-      {/* 4. Faixa de Valores (Marquee de texto) */}
+      {/* 4. Faixa de Valores (Texto correndo) */}
       <ValuesMarquee />
 
-      {/* 5. Chamada para o plano Dale+ */}
+      {/* 5. Programa de Indicação */}
+      <DaleIndicaSection />
+
+      {/* 6. Chamada Dale+ */}
       <DalePlusTeaser />
 
-      {/* 6. Seção Institucional (Sobre) */}
+      {/* 7. Seção Sobre (Institucional) */}
       <AboutSection />
 
-      {/* 7. Depoimentos de Clientes */}
+      {/* 8. Depoimentos dos Clientes (Prova Social) */}
       <ReviewsSection />
 
-      {/* 8. Mapa e Localização */}
+      {/* 9. Localização e Mapa */}
       <LocationSection />
     </main>
   );
